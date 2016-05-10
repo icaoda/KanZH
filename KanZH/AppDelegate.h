4d7d456b5982804056f3ecd809a6ec7d272aa779
SW05 <sw05@sw05.local>
2016-05-10 08:02:12 +0800

    Initial Commit

---
+++ KanZH/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KanZH
+//
+//  Created by SW05 on 5/10/16.
+//  Copyright © 2016 SW05. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KanZH/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KanZH
+//
+//  Created by SW05 on 5/10/16.
+//  Copyright © 2016 SW05. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
